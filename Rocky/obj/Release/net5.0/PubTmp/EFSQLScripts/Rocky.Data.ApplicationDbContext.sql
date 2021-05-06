76698cb027fd51fb455c22984952f08c33adf4c5
damolaajayi <damolaajayi@gmail.com>
2021-05-06 17:36:10 +0100

    changes

--- Rocky/obj/Release/net5.0/PubTmp/EFSQLScripts/Rocky.Data.ApplicationDbContext.sql
+++ Rocky/obj/Release/net5.0/PubTmp/EFSQLScripts/Rocky.Data.ApplicationDbContext.sql
@@ -55,3 +55,184 @@ GO
 COMMIT;
 GO
 
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210430214246_AddRequiredFields')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Category]') AND [c].[name] = N'Name');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Category] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Category] ALTER COLUMN [Name] nvarchar(max) NOT NULL;
+    ALTER TABLE [Category] ADD DEFAULT N'' FOR [Name];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210430214246_AddRequiredFields')
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[ApplicationType]') AND [c].[name] = N'Name');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [ApplicationType] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [ApplicationType] ALTER COLUMN [Name] nvarchar(max) NOT NULL;
+    ALTER TABLE [ApplicationType] ADD DEFAULT N'' FOR [Name];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210430214246_AddRequiredFields')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210430214246_AddRequiredFields', N'5.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210430214747_AddProductToDb')
+BEGIN
+    CREATE TABLE [Product] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Description] nvarchar(max) NULL,
+        [Price] float NOT NULL,
+        [image] nvarchar(max) NULL,
+        [CategoryId] int NOT NULL,
+        CONSTRAINT [PK_Product] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Product_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210430214747_AddProductToDb')
+BEGIN
+    CREATE INDEX [IX_Product_CategoryId] ON [Product] ([CategoryId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210430214747_AddProductToDb')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210430214747_AddProductToDb', N'5.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503162053_AddApplicationTypeToProduct')
+BEGIN
+    ALTER TABLE [Product] ADD [ApplicationId] int NOT NULL DEFAULT 0;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503162053_AddApplicationTypeToProduct')
+BEGIN
+    ALTER TABLE [Product] ADD [ApplicationTypeId] int NULL;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503162053_AddApplicationTypeToProduct')
+BEGIN
+    CREATE INDEX [IX_Product_ApplicationTypeId] ON [Product] ([ApplicationTypeId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503162053_AddApplicationTypeToProduct')
+BEGIN
+    ALTER TABLE [Product] ADD CONSTRAINT [FK_Product_ApplicationType_ApplicationTypeId] FOREIGN KEY ([ApplicationTypeId]) REFERENCES [ApplicationType] ([Id]) ON DELETE NO ACTION;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503162053_AddApplicationTypeToProduct')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210503162053_AddApplicationTypeToProduct', N'5.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503165718_AddApplicationTypeToProduct_Rename')
+BEGIN
+    ALTER TABLE [Product] DROP CONSTRAINT [FK_Product_ApplicationType_ApplicationTypeId];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503165718_AddApplicationTypeToProduct_Rename')
+BEGIN
+    DECLARE @var2 sysname;
+    SELECT @var2 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Product]') AND [c].[name] = N'ApplicationId');
+    IF @var2 IS NOT NULL EXEC(N'ALTER TABLE [Product] DROP CONSTRAINT [' + @var2 + '];');
+    ALTER TABLE [Product] DROP COLUMN [ApplicationId];
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503165718_AddApplicationTypeToProduct_Rename')
+BEGIN
+    DROP INDEX [IX_Product_ApplicationTypeId] ON [Product];
+    DECLARE @var3 sysname;
+    SELECT @var3 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Product]') AND [c].[name] = N'ApplicationTypeId');
+    IF @var3 IS NOT NULL EXEC(N'ALTER TABLE [Product] DROP CONSTRAINT [' + @var3 + '];');
+    ALTER TABLE [Product] ALTER COLUMN [ApplicationTypeId] int NOT NULL;
+    ALTER TABLE [Product] ADD DEFAULT 0 FOR [ApplicationTypeId];
+    CREATE INDEX [IX_Product_ApplicationTypeId] ON [Product] ([ApplicationTypeId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503165718_AddApplicationTypeToProduct_Rename')
+BEGIN
+    ALTER TABLE [Product] ADD CONSTRAINT [FK_Product_ApplicationType_ApplicationTypeId] FOREIGN KEY ([ApplicationTypeId]) REFERENCES [ApplicationType] ([Id]) ON DELETE CASCADE;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210503165718_AddApplicationTypeToProduct_Rename')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210503165718_AddApplicationTypeToProduct_Rename', N'5.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210504125822_AddShortDescToProductTable')
+BEGIN
+    ALTER TABLE [Product] ADD [ShortDesc] nvarchar(max) NULL;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210504125822_AddShortDescToProductTable')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210504125822_AddShortDescToProductTable', N'5.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
